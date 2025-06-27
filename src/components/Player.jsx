59b0e680e9ac8442a7193f63ce6258ca37d6069d
Drishti Shah <drishtishah2004@gmail.com>
2025-06-27 18:40:58 +0530

    Updating player name and button name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
+
+
 export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing((prev) => !prev);
+    }
+
+    let playerName = <span className="player-name">{name}</span>
+    let buttonName = "Edit"
+
+    if (isEditing) {
+        playerName = <input type="text" required />
+        buttonName = "Save"
+    }
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">Player 1</span>
-                <span className="player-symbol">X</span>
+                {playerName}
+                <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
-
-            <button></button>
+            <button onClick={handleEditClick}>{buttonName}</button>
         </li>
     );
+
+
+
+
 }
