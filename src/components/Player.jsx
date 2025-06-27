cbd3d21b7fb099d0967b59be920009c74cb29f67
Drishti Shah <drishtishah2004@gmail.com>
2025-06-27 19:18:42 +0530

    fixes

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,15 +5,16 @@ export default function Player({ name, symbol }) {
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
+        // setIsEditing(!isEditing); => schedules a state update to true
         setIsEditing((prev) => !prev);
     }
 
     let playerName = <span className="player-name">{name}</span>
-    let buttonName = "Edit"
+    //let btnCaption = "Edit"
 
     if (isEditing) {
-        playerName = <input type="text" required />
-        buttonName = "Save"
+        playerName = <input type="text" required value={name} />
+        //btnCaption = "Save"
     }
 
     return (
@@ -22,7 +23,7 @@ export default function Player({ name, symbol }) {
                 {playerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>{buttonName}</button>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     );
 
